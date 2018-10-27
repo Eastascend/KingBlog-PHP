953981b70994bc4c52e155ec05ca5a2960987560
Eastascend <947747330@qq.com>
2018-10-28 00:08:18 +0800

    kingblog #V1.0.0-网站管理

---
+++ application/admin/lang/zh-cn/banner.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+    'Id'  =>  'ID',
+    'Name'  =>  '名称',
+    'Image'  =>  '图片',
+    'Weigh'  =>  '排序',
+    'Link'  =>  '链接',
+    'Status'  =>  '状态',
+    'Status 0'  =>  '未上架',
+    'Status 1'  =>  '上架',
+    'Createtime'  =>  '创建时间',
+    'Updatetime'  =>  '修改时间'
+];
