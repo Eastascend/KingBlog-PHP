7c8ee576e0022f45af2e52a6e2aeddf14b6c82b0
Eastascend <947747330@qq.com>
2018-10-29 00:43:37 +0800

    kingblog #V1.0.0-样式调整

--- application/index/controller/About.php
+++ application/index/controller/About.php
@@ -2,6 +2,7 @@
 
 namespace app\index\controller;
 
+use app\admin\model\article\Category;
 use app\common\controller\Frontend;
 use app\common\library\Token;
 
@@ -11,14 +12,23 @@ class About extends Frontend
     protected $noNeedLogin = '*';
     protected $noNeedRight = '*';
     protected $layout = 'common';
+    protected $categoryModel = null;
 
     public function _initialize()
     {
+        $this->categoryModel = new Category();
         parent::_initialize();
     }
 
     public function index()
     {
+        // 博客类别
+        $categories = $this->categoryModel
+            ->order('id')
+            ->select();
+
+        $this->view->assign("categories", $categories);
+
         return $this->view->fetch('/about-me');
     }
 
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -2,6 +2,7 @@
 
 namespace app\index\controller;
 
+use app\admin\model\article\Category;
 use app\admin\model\article\Tag;
 use app\admin\model\Banner;
 use app\common\controller\Frontend;
@@ -14,12 +15,14 @@ class Index extends Frontend
     protected $noNeedRight = '*';
     protected $layout = 'common';
     protected $bannerModel = null;
+    protected $categoryModel = null;
     protected $tagsModel = null;
 
     public function _initialize()
     {
         parent::_initialize();
         $this->bannerModel = new Banner();
+        $this->categoryModel = new Category();
         $this->tagsModel = new Tag();
     }
 
@@ -32,12 +35,18 @@ class Index extends Frontend
             ->limit(5)
             ->select();
 
+        // 博客类别
+        $categories = $this->categoryModel
+            ->order('id')
+            ->select();
+
         // 标签
         $tags = $this->tagsModel
             ->order('id')
             ->select();
 
         $this->view->assign("banners", $banners);
+        $this->view->assign("categories", $categories);
         $this->view->assign("tags", $tags);
 
         return $this->view->fetch('/index');
