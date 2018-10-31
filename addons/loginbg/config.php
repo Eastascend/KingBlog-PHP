11cf47dea0807b03b0e9c65fb1a14da8df6f6719
Eastascend <947747330@qq.com>
2018-11-01 01:41:27 +0800

    no message

---
+++ addons/loginbg/config.php
@@ -0,0 +1,37 @@
+<?php
+
+return array (
+  0 => 
+  array (
+    'name' => 'mode',
+    'title' => '模式',
+    'type' => 'radio',
+    'content' => 
+    array (
+      'fixed' => '固定',
+      'random' => '每次随机',
+      'daily' => '每日切换',
+    ),
+    'value' => 'daily',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+  1 => 
+  array (
+    'name' => 'image',
+    'title' => '固定背景图',
+    'type' => 'image',
+    'content' => 
+    array (
+    ),
+    'value' => '/uploads/20181028/5da6f71345280088e509902225502801.jpg',
+    'rule' => 'required',
+    'msg' => '',
+    'tip' => '',
+    'ok' => '',
+    'extend' => '',
+  ),
+);
